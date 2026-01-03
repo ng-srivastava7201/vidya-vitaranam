9ec1e33c0c57c6a7faa99f9fd10800478a0be1e1
ng-srivastava7201 <nityagsrivastava@gmail.com>
2026-01-03 09:53:10 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
@@ -9,4 +10,12 @@ class User(db.Model, UserMixin):
   name = db.Column(db.String(150))
   school = db.Column(db.String(150))
   role = db.Column(db.String(10), default='student')
-  
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(100))
+    chapter = db.Column(db.String(100))
+    filename = db.Column(db.String(200))   
+    youtube_link = db.Column(db.String(300))
+    teacher_name = db.Column(db.String(150))
+    upload_date = db.Column(db.DateTime(timezone=True), default=func.now())
